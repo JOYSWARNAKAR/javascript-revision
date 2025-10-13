5c7f66afa7b4afb1110ebd8e8b7ae9cbc869fcd7
Joy Swarnakar <123759014+JOYSWARNAKAR@users.noreply.github.com>
2025-10-13 15:15:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+// JS engine is present in the browser
+// code should be readable
+
+// console.log(3+3); // code readability should be high
+// console.log("hello");
+
+let name = "joy"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof"joy"); //string
+console.log(typeof age);// number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
+
+
+
+
