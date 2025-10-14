a8180ed31ed01c6a62f067fc8ea9ec2bea19bcf3
Joy Swarnakar <123759014+JOYSWARNAKAR@users.noreply.github.com>
2025-10-14 16:48:04 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,94 @@
+let score = "hello"
+
+
+// console.log(typeof score); 
+// console.log(typeof(score)); 
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+let isLoggedIn = "hello"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true => 1; false=> 0
+// "" => false
+// "hello" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+// ***********************Operations****************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);  // divisor
+// console.log(2%3); // remainder
+
+let str1 ="hello"
+let str2 =" world"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2+2); // If string came first then evrything is string
+// console.log((1 + 2) +"2");
+
+// code should be simple and easy to readable.
+
+// console.log(true);
+
+// let num1,num2,num3
+// num1 = num2 = num3= 2+2
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter);
+// learn more: prefix and postfix mdn docs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
