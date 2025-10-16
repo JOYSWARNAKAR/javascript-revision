726b3c8914af7a107fa36f48262be27461a1c1c3
Joy Swarnakar <123759014+JOYSWARNAKAR@users.noreply.github.com>
2025-10-16 11:12:36 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,50 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1)
+// console.log(2!=1);
+;
+
+// console.log("2">1); // string converted to number
+// console.log("02">1); // string converted to number
+
+// Typesript donot allow you to comparison datatypes
+// TS is like a framework, follow strict rule
+
+// JS will not give the expectation result in comparison of datatypes
+
+// console.log(null > 0);
+// console.log(null ==0);
+// console.log(null >= 0);
+
+console.log( undefined == 0);
+console.log(undefined >0);
+console.log(undefined < 0);
+
+
+// console.log("2"===2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
