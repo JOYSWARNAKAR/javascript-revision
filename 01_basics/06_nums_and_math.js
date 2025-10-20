c23e68585ac4e5ad75282c6a8defd0ef2b472bac
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2025-10-21 02:56:35 +0530

    hello

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // for building ecommerce app
+
+const otherNumber = 125.369
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreads = 1000000
+// console.log(hundreads.toLocaleString());
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
+console.log(Math); // Object {}
+console.log(Math.abs(-4)); // 4
+console.log(Math.round(4.4)); // 4 // mostly used
+console.log(Math.ceil(4.2));  // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(4,8,6,5,2,12));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
