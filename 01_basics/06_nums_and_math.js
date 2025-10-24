4edcfceccb6082121a60f7f3671f7bf5a7ae30d1
Joy Swarnakar <123759014+JOYSWARNAKAR@users.noreply.github.com>
2025-10-24 11:00:33 +0000

    math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -13,21 +13,26 @@ const otherNumber = 125.369
 
 const hundreads = 1000000
 // console.log(hundreads.toLocaleString());
-// console.log(hundreads.toLocaleString('en-IN'));
+// console.log(hundreads.toLocaleString('en-IN'));  //
 
 // +++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
 
-console.log(Math); // Object {}
-console.log(Math.abs(-4)); // 4
-console.log(Math.round(4.4)); // 4 // mostly used
-console.log(Math.ceil(4.2));  // 5
-console.log(Math.floor(4.9)); // 4
-console.log(Math.min(4,8,6,5,2,12));
+// console.log(Math); // Object {}
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.4)); // 4 // mostly used
+// console.log(Math.ceil(4.2));  // 5
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.min(4,8,6,5,2,12));  // 2
+// console.log(Math.max(4,8,6,5,2,12)); // 12
 
+console.log(Math.random()); // value lie betwween 0 to 1
+console.log((Math.random()*10) + 1); // 1.0000... - 10.00000....
+console.log(Math.floor(Math.random()*10) + 1 ); // 1 - 10, we add +1 because sometime the value return 0, but we need value 1 to 10 range
 
+const min = 10
+const max = 20
 
-
-
+console.log(Math.floor(Math.random() * (max-min +1)) + min ) // range 10 to 20
 
 
 
